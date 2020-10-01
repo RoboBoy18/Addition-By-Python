9d5c230a99e4142a8aed8a56f68fd37c44569141
Veena Patel <72180174+RoboBoy18@users.noreply.github.com>
2020-10-01 15:25:48 +0530

    Delete Combinations.py

--- Combinations.py
+++
@@ -1,5 +0,0 @@
-import math
-total=int(input('Enter total number of outcomes:'))
-choose=int(input('\nEnter number of items to choose:'))
-result=math.comb(total,choose)
-print('\nTotal combinations:'+str(result))
